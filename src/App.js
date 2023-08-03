83623d417e81c9bc4defba91482b3772f058bf63
rishikanchi <rishikanchi@programmer.net>
2023-08-03 12:14:49 -0500

    hector

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hector is an illegal immigrant!
         </p>
         <a
           className="App-link"
