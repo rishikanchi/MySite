b6c0f3702a9c3ff34b3d4aca72d7cca5f6c5b3af
rishikanchi <rishikanchi@programmer.net>
2023-08-03 23:22:48 -0500

    Created NavBar and _redirects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hector is an illegal immigrant!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id = "App">
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import hamburger from '../images/hamburger.png'
+import rk_logo from '../images/rk_logo.png'
+import './Navbar.css'
+
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={rk_logo} id='logoPic'></img>
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <img src={hamburger} id='hamPic'></img>
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
