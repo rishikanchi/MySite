22d2c1513b0db56fa5a63d5cf10d019849963da8
rishikanchi <rishikanchi@programmer.net>
2023-08-04 17:50:23 -0500

    Added Intro: Name + Headshot

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
-import { BrowserRouter } from 'react-router-dom'
+import Intro from './components/Intro';
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
       </BrowserRouter>
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,19 @@
+import "./Intro.css";
+import { React } from 'react'
+import rishi_nobg from "../images/rishi_nobg.png";
+
+function Intro() {
+    return (
+        <div id='intro'>
+            <div id="nameCont">
+                <div id='hello'>Hello, my name is</div>
+                <div id='name' className="name">Rishi Kanchi<span id='dot' className="name"></span></div>
+            </div>
+            <div id='picCont'>
+                <img src={rishi_nobg} id="headshot"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Intro;
