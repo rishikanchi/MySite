a8743b49c7dee885b1ba37d2123da717dbc456f2
rishikanchi <rishikanchi@programmer.net>
2023-08-04 22:53:22 -0500

    Added typewriter

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,13 +1,33 @@
 import "./Intro.css";
-import { React } from 'react'
+import { React, useState } from 'react'
 import rishi_nobg from "../images/rishi_nobg.png";
+import Typewriter from 'typewriter-effect';
+
 
 function Intro() {
+
     return (
         <div id='intro'>
             <div id="nameCont">
-                <div id='hello'>Hello, my name is</div>
-                <div id='name' className="name">Rishi Kanchi<span id='dot' className="name"></span></div>
+                <div className='hello'>Hello, my name is</div>
+                <div className="name">Rishi Kanchi<span id='dot'></span></div>
+                <div className='hello'>I am a</div>
+                <span className="name" style={{fontSize: '62px', margin: 0}}><Typewriter
+                    options={{
+                        strings: [
+                                    'Web Designer' + "<span id='dot'></span>",
+                                    'Scholar' + "<span id='dot'></span>",
+                                    'Mathlete' + "<span id='dot'></span>", 
+                                    'Teacher' + "<span id='dot'></span>",
+                                    'Chess Player' + "<span id='dot'></span>",
+                                    'Volunteer' + "<span id='dot'></span>",
+                                    'Data Scientist' + "<span id='dot'></span>",
+                                    'Engineer'+ "<span id='dot'></span>"
+                                ],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                /></span>
             </div>
             <div id='picCont'>
                 <img src={rishi_nobg} id="headshot" alt="headshot"></img>
