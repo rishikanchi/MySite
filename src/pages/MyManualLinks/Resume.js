257357bf13cc2062ab7d7d4bc95fa12a54f4eb14
rishikanchi <98680362+rishikanchi@users.noreply.github.com>
2024-02-18 16:48:02 -0600

    update resume link

--- src/pages/MyManualLinks/Resume.js
+++ src/pages/MyManualLinks/Resume.js
@@ -1,5 +1,5 @@
 function Resume() {
-    window.location.href = 'https://drive.google.com/file/d/1geqX7X4WpHzCf91lPROTVfgX3WovLw4r/view?usp=sharing';
+    window.location.href = 'https://drive.google.com/file/d/11czp0cHYqpwkKdffgW_kp0k8UuuioLtx/view?usp=sharing';
 }
 
 export default Resume;
